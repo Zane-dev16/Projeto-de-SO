67eb0f5772944665f4206e912613fad7307ba3d1
Irell Zane <itsirellzane@gmail.com>
2023-12-15 19:25:27 +0000

    optimize parallelization in show

--- p1_base/operations.c
+++ p1_base/operations.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
+#include <limits.h>
 
 #include "eventlist.h"
 
@@ -180,8 +181,6 @@ int ems_reserve(unsigned int event_id, size_t num_seats, size_t* xs, size_t* ys)
     return 1;
   }
 
-  pthread_rwlock_rdlock(&event->event_lock);
-
   size_t i = 0;
   int can_reserve = 1;
   for (; i < num_seats; i++) {
@@ -211,7 +210,6 @@ int ems_reserve(unsigned int event_id, size_t num_seats, size_t* xs, size_t* ys)
       *get_seat_with_delay(event, seat_index(event, row, col)) = reservation_id;
       pthread_rwlock_unlock(&event->seatlocks[seat_index(event, row, col)]);
     }
-    pthread_rwlock_unlock(&event->event_lock);
     return 0;
   }
   else {
@@ -220,7 +218,6 @@ int ems_reserve(unsigned int event_id, size_t num_seats, size_t* xs, size_t* ys)
       size_t col = ys[j];
       pthread_rwlock_unlock(&event->seatlocks[seat_index(event, row, col)]);
     }
-    pthread_rwlock_unlock(&event->event_lock);
     return 1;
   }
 }
@@ -238,23 +235,44 @@ int ems_show(unsigned int event_id, int fd) {
     fprintf(stderr, "Event not found\n");
     return 1;
   }
-  pthread_rwlock_wrlock(&event->event_lock);
-  pthread_mutex_lock(&output_lock);
-  for (size_t i = 1; i <= event->rows; i++) {
-    for (size_t j = 1; j <= event->cols; j++) {
-      unsigned int* seat = get_seat_with_delay(event, seat_index(event, i, j));
-      char seatStr[12];
-      snprintf(seatStr, sizeof(seatStr), "%u", *seat);
-      write(fd, seatStr, strlen(seatStr));
+  size_t max_seat_length = (size_t)snprintf(NULL, 0, "%u", UINT_MAX); // Maximum seatStr length
+  size_t buffer_size = (event->rows * event->cols * (max_seat_length + 1)) + event->rows; // +event->rows for newline characters
+  char *buffer = malloc(buffer_size);
 
-      if (j < event->cols) {
-        write(fd, " ", strlen(" "));
+  if (buffer == NULL) {
+      // Handle memory allocation failure
+      // Add error handling code here
+  } else {
+      size_t buffer_position = 0;
+
+      for (size_t i = 1; i <= event->rows; i++) {
+          for (size_t j = 1; j <= event->cols; j++) {
+              pthread_rwlock_rdlock(&event->seatlocks[seat_index(event, i, j)]);
+              unsigned int* seat = get_seat_with_delay(event, seat_index(event, i, j));
+              pthread_rwlock_unlock(&event->seatlocks[seat_index(event, i, j)]);
+              size_t len = (size_t)snprintf(buffer + buffer_position, max_seat_length + 1, "%u", *seat);
+              buffer_position += len;
+
+              // Add space unless it's the last column
+              if (j < event->cols) {
+                  buffer[buffer_position] = ' ';
+                  buffer_position++;
+              }
+          }
+          
+          // Add newline after each row
+          buffer[buffer_position] = '\n';
+          buffer_position++;
       }
-    }
-    write(fd, "\n", strlen("\n"));
+
+      // Write the entire buffer to fd
+      pthread_mutex_lock(&output_lock);
+      write(fd, buffer, buffer_position);
+      pthread_mutex_unlock(&output_lock); 
+
+      // Free allocated memory
+      free(buffer);
   }
-  pthread_mutex_unlock(&output_lock);
-  pthread_rwlock_unlock(&event->event_lock);
 
   return 0;
 }
